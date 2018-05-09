5ca2aa2c73614454a27dc1e2983cc9ddc60a0a4f
tcdsv <33223663+tcdsv@users.noreply.github.com>
2018-05-09 16:00:21 +0300

    Add files via upload

--- worker.py
+++ worker.py
@@ -2,123 +2,24 @@ import pika
 import sys
 import threading
 import time
+import manager
 
 args = sys.argv[1:]
+manager.worker_routing_key=args[0]
 
-worker_queue_key = args[0]
-
-def bind(routing_key):
-    workers_channel.queue_bind(exchange='workers',
-                               queue=workers_queue_name,
-                               routing_key=routing_key)
-
-def unbind(routing_key):
-    workers_channel.queue_unbind(exchange='workers',
-                               queue=workers_queue_name,
-                               routing_key=routing_key)
-
-def bind_neighbours(neighbours):
-    for neighbour in neighbours:
-        bind(neighbour)
-
-def send(routing_key, message):
-    workers_channel.basic_publish(exchange='workers',
-                          routing_key=routing_key,
-                          body=message)
-
-
-def create_connection():
-    return pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
-
-def create_channel(connection, exchange_name, exchange_type):
-    channel = connection.channel()
-    channel.exchange_declare(exchange=exchange_name,
-                         exchange_type=exchange_type)
-    return channel
-
-control_connection = create_connection()
-control_channel = create_channel(control_connection,'control','topic')
-
-# declare control queue
-control_result = control_channel.queue_declare(exclusive=True)
-control_queue_name = control_result.method.queue
-
-# bind worker to exchange
-control_channel.queue_bind(exchange='control',
-                   queue=control_queue_name,
-                   routing_key=args[0])
-
-workers_connection = create_connection()
-workers_channel = create_channel(workers_connection,'workers','topic')
-
-# channel for worker to worker communication
-workers_channel = workers_connection.channel()
-workers_channel.exchange_declare(exchange='workers',
-                         exchange_type='topic')
-
-# declare workers queue
-workers_result = workers_channel.queue_declare(exclusive=True)
-workers_queue_name = workers_result.method.queue
-
-# workers_queue_name = workers_result.method.queue
-
-
-
-
-
-
-def control_callback(ch, method, properties, body):
-    message = body.decode()
-    message = message.split()
-
-    if message[0] == 'send':
-
-        send(args[0],' '.join(message[1:]))
-        print("sent with routing_key: ", args[0])
-        print("message: ",' '.join(message[1:]))
-    else:
-        print(" [x] %r:%r" % (method.routing_key, body))
-
-"""
-    if body.decode() == "send":
-        send(args[0],"this is a message from worker to worker")
-        print("sned message from: ", args[0])
-"""
-
-def workers_callback(ch, method, properties, body):
-    print(" [x] %r:%r" % (method.routing_key, body))
-
-    message = body.decode()
-    message = message.split()
-
-    if message[0] == args[0]: #message is for me:
-        print("received a message for me!")
-    else:
-        print("this message is not for me: ", ' '.join(message))
-
-control_channel.basic_consume(control_callback,
-                      queue=control_queue_name,
-                      no_ack=True)
-
-
-workers_channel.basic_consume(workers_callback,
-                      queue=workers_queue_name,
-                      no_ack=True)
-
-
-# user to worker communication consumer thread
+manager.run()
 
 binding_keys = args[1:]
 print("neighbours:", binding_keys)
 
-bind_neighbours(binding_keys)
+manager.bind_neighbours(binding_keys)
 
 
 def consume_control():
-    control_channel.start_consuming()
+    manager.control_channel.start_consuming()
 
 def consume_workers():
-    workers_channel.start_consuming()
+    manager.workers_channel.start_consuming()
 
 
 control_thread = threading.Thread(target=consume_control)
@@ -130,14 +31,7 @@ workers_thread.start()
 while True:
     time.sleep(3)
     print("working...")
-
-
-
-
-
-
-
-
+    print(manager.worker_routing_key)
 
 
 
