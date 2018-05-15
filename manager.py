a2030ae13aedcfc14f6aceac5eca84862a43467b
tcdsv <33223663+tcdsv@users.noreply.github.com>
2018-05-15 14:52:00 +0300

    message serialization with pickle

--- manager.py
+++ manager.py
@@ -1,4 +1,5 @@
 import pika
+import pickle
 
 class manager(object):
     def __init__(self, mq_server_address, exchange_name, exchange_type, routing_key):
@@ -35,15 +36,22 @@ class worker_manager(manager):
 
     def callback(self,ch, method, properties, body):
 
-        print(" [x] %r:%r" % (method.routing_key, body))
+        # print(" [x] %r:%r" % (method.routing_key, body))
 
-        message = body.decode()
-        message = message.split()
+        # message = body.decode()
+        # message = message.split()
 
+        message = pickle.loads(body)
+        print("received:")
+        print(message)
+
+
+        """
         if message[0] == self.routing_key: #message is for me:
             print("received a message for me!")
         else:
             print("this message is not for me: ", ' '.join(message))
+        """
 
 
     # bind worker to neighbour queue
@@ -65,6 +73,11 @@ class worker_manager(manager):
         for neighbour in neighbours:
             self.bind(neighbour)
 
+    """
+    def executor(self,message, func=basic_strategy):
+        if func is None:
+            print("strategy not set")
+    """
 
 class control_manager(manager):
     def __init__(self,mq_server_address, exchange_name, exchange_type, routing_key):
@@ -82,9 +95,30 @@ class control_manager(manager):
         print("im here")
         if message[0] == 'send':
 
+
+            # message serialization test
+
+            msg = {'source':self.routing_key,
+                   'destination':'dummy_destination',
+                   'TTL':10,
+                   'data':' '.join(message[1:])}
+
+            print("sending:")
+            print(msg)
+
+
+            msg=pickle.dumps(msg)
+
+            self.send(self.routing_key,msg,'workers')
+
+
+            """
             self.send(self.routing_key,' '.join(message[1:]),"workers")
             print("sent with routing_key: ", self.routing_key)
             print("message: ",' '.join(message[1:]))
+            """
+
+
         else:
             print(" [x] %r:%r" % (method.routing_key, body))
 
