c0f1e3ebc61ec0be0dd4cdf3a5ce6744659ea5e6
ehrlichelman <micahehrlich@gmail.com>
2018-05-16 21:29:24 +0300

    Add files via upload
    Added remove function for removing neighbour node.

--- address_db.py
+++ address_db.py
@@ -13,47 +13,48 @@ class AddressDictionary:
         addr = self.redis_db.srandmember('addr_list')
         self.redis_db.srem('addr_list', addr)
         self.redis_db.set(name, addr)
-        #print('selected address: ' + str(self.redis_db.get(name))[2:-1])
-        #print('remaining addresses: ' + str(self.redis_db.smembers('addr_list')))
 
     def remove_name(self, name):
         addr = self.redis_db.get(name)
         self.redis_db.delete(name)
         self.redis_db.sadd('addr_list', addr)
-        #print('remaining addresses: ' + str(self.redis_db.smembers('addr_list')))
 
+    # returns node address
     def value_by_name(self, name):
         address = str(self.redis_db.get(name))[2:-1]
         print('address: ' + address)
         return address
 
+    # returns list of addresses for nodes
     def list_of_addresses(self, l_name=[]):
-        #print(str(l_name))
         l_addr = []
         for x in l_name:
             print(self.value_by_name(str(x)))
             l_addr.append(self.value_by_name(x))
         return l_addr
 
-    def get_all(self):
-        count_keys = 0
-        for name in self.redis_db.keys():
-            if count_keys < len(self.redis_db.keys())-1:
-                name = str(name)[2:-1]
-                addr = str(self.redis_db.get(name))[2:-1]
-                print('name: ' + name + ' address: ' + addr)
-            count_keys += 1
-
+    # add node neighbour
     def add_node_neighbour(self, node, neighbour):
         self.redis_db.sadd(node, neighbour)
         self.print_node_neighbours(node)
 
+    # remove connecting edge between nodes
+    def remove_edge(self, node, neighbour):
+        self.redis_db.srem(node, neighbour)
+        self.redis_db.srem(neighbour, node)
+
+    # disconnect node from all neighbours
+    def remove_node_neighbours(self, node):
+        neighbours_l = self.node_neighbours_list(node)
+        for x in neighbours_l:
+            self.remove_edge(node, x)
+
     def print_node_neighbours(self, node):
-        neighbours = str(self.redis_db.smembers(node))
+        neighbours = str(self.redis_db.smembers(node)).replace("b'", "").replace("'", "")
         print('Node Address: ' + node + ' Neighbours: ' + neighbours)
 
     def node_neighbours_list(self, node):
-        neighbours_l = str(self.redis_db.smembers(node)).split(",")
+        neighbours_l = str(self.redis_db.smembers(node)).replace("b'", "").replace("'", "").split(",")
         return neighbours_l
 
     def drop_db(self):
@@ -63,7 +64,7 @@ class AddressDictionary:
 if __name__ == '__main__':
     redis_data = AddressDictionary()
     while True:
-        user_input = int(input('1 - add, 2 - remove, 3 - find by name, 4 - neighbours: \n'))
+        user_input = int(input('1 - add, 2 - remove, 3 - find by name, 4 - neighbours, 5 - remove neighbour: \n'))
         if user_input == 1:
             name = input('enter name:')
             redis_data.add_name_addr(name)
@@ -76,6 +77,10 @@ if __name__ == '__main__':
         elif user_input == 4:
             address = input('enter address:')
             redis_data.print_node_neighbours(address[2:-1])
+        elif user_input == 5:
+            node = input('enter node address:')
+            neighbour = input('enter neighbour address:')
+            redis_data.remove_edge(node, neighbour)
         else:
             break
 
